0330d5c22e9a866c6cd6345a5d2d9110f1b96720
KenSa0day <108104817+Kensa0day@users.noreply.github.com>
2023-08-21 23:23:18 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './App.scss';
+
+import { BrowserRouter, Route } from '.react-router-dom';
+
+import Header from './src/components/header/Header'
+
+import Routes from './src/config/Routes';
+import Footer from './src/components/footer';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Route render={props => (
+                <>
+                    <Header {...props}/>
+                    <Routes/>
+                    <Footer/>
+                </>
+            )}/>
+        </BrowserRouter>
+    );
+}
+
+export default App;
