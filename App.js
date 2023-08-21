4e5a8b5876c014be38cfcafa693cb193d8f94d8e
KenSa0day <108104817+Kensa0day@users.noreply.github.com>
2023-08-21 23:24:53 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'swiper/swiper.min.css';
 import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
 
-import { BrowserRouter, Route } from '.react-router-dom';
+import { BrowserRouter, Route } from './node_modules/react-router-dom';
 
 import Header from './src/components/header/Header'
 
